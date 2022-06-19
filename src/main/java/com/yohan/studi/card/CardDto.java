aab0d55271e23e33da8584d678654b3e06b76d40
Yo Han Joo <yohan.arbete@gmail.com>
2022-06-19 10:37:15 +0200

    add due date

--- src/main/java/com/yohan/studi/card/CardDto.java
+++ src/main/java/com/yohan/studi/card/CardDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,12 +13,14 @@ public class CardDto {
     private Integer id;
     private String question;
     private String answer;
+    private Date dueDate;
     private Integer subjectId;
 
     public CardDto(Card card) {
         this.id = card.getId();
         this.question = card.getQuestion();
         this.answer = card.getAnswer();
+        this.dueDate = card.getDueDate();
         this.subjectId = card.getSubject().getId();
     }
 }
